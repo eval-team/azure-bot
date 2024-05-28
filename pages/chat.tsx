f1425ea0f86722d672f53bc222b52052d6f76dbe
nisar <nisar2048@gmail.com>
2024-05-28 19:05:07 +0530

    new page and api

---
+++ pages/chat.tsx
@@ -0,0 +1,14 @@
+// pages/chat.tsx
+
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      <p>Welcome to the chat page!</p>
+    </div>
+  );
+};
+
+export default Chat;
