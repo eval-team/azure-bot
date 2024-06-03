b788594c412fa53ee4cff0987bac92f0ca82e617
sameer-ku-sahu <sameersahu@eval.team>
2024-06-03 12:35:39 +0530

    system-message update

--- pages/system-message.tsx
+++ pages/system-message.tsx
@@ -14,6 +14,7 @@ const systemMessage = () => {
       message: input,
       user: "system",
     });
+    setInput("");
   };
 
   return (
@@ -23,7 +24,7 @@ const systemMessage = () => {
           rows={4}
           name="comment"
           id="comment"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
